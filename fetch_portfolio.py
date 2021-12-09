749b511b1a1efcfb90bd28ae03e73cb686c3ce16
rohit sohlot <5600987+rsohlot@users.noreply.github.com>
2021-12-09 23:27:55 +0530

    add notebook for testing code and day class

--- fetch_portfolio.py
+++ fetch_portfolio.py
@@ -1,9 +1,19 @@
 import yfinance as yf
 
+class DayPrice:
+    def __init__(self) -> None:
+        self.date = None
+        self.open = None
+        self.close = None
+        self.high = None
+        self.low = None
+        
+
 class Stock:
     def __init__(self, stock_code:str, stock_name = None) -> None:
         self.stock_code  = stock_code
-        self.stock_price = None
+        # list of Day price
+        self.stock_price:list = []
 
     def fetch_stock_historical_price(self):
         self.stock_price = yf.downlaod(self.stock_code)
