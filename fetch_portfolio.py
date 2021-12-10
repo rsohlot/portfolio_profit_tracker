70327a194bf38ac72c47a9193aff9bbdb6c9f1bb
rsohlot <5600987+rsohlot@users.noreply.github.com>
2021-12-10 12:37:13 +0530

    import bug resolved, portfolio load function completed

--- fetch_portfolio.py
+++ fetch_portfolio.py
@@ -1,7 +1,8 @@
 import yfinance as yf
 import pandas as pd
-from datetime.datetime import date
+from datetime import date
 import glob
+import json
 
 class DayPrice:
     def __init__(self) -> None:
@@ -13,9 +14,9 @@ class DayPrice:
         
 
 class Stock:
-    def __init__(self, stock_code:str,qnty:int,date_purchased:date, stock_name = None) -> None:
+    def __init__(self, stock_code:str,quantity:int,date_purchased:date, stock_name = None) -> None:
         self.stock_code  = stock_code
-        self.qnty = qnty
+        self.quantity = quantity
         self.date_purchased = date_purchased
         # list of Day price
         self.stock_price:list = []
@@ -32,12 +33,15 @@ class Portfolio:
     def add_stock(self, stock: Stock):
         self.stock_list.append(stock)
 
-    def load(source='zerodha', path = 'data/reports/'):
+    def load(self, source='zerodha', path = 'data/reports/'):
         """
         Load all the csv in the folder data/reports
         """
         data =  pd.concat(map(pd.read_csv, glob.glob(path + "/*.csv"))) 
-
+        data_json = json.loads(data.to_json(orient = 'records'))
+        for each_stock in data_json:
+            stock = Stock(each_stock['symbol'],each_stock['quantity'],each_stock['trade_date'],each_stock['symbol'])
+            self.stock_list.append(stock)
         # create portfolio objects from the data
 
     
--- profit_plotter.py
+++ profit_plotter.py
@@ -1,6 +1,6 @@
 from fetch_portfolio import Portfolio
 
 # create portfolio
-portfolio = Portfolio()
+portfolio = Portfolio(portfolio_name='my_investment_portfolio')
 # load the data
 portfolio.load()
