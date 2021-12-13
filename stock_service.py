05ce5e3e38f706ddc1def0730731fd6d210e28a1
rsohlot <5600987+rsohlot@users.noreply.github.com>
2021-12-14 00:08:56 +0530

    changing the column used for date and fill record for sat and sun

--- stock_service.py
+++ stock_service.py
@@ -11,6 +11,37 @@ stock_table_columns = ['_id','symbol', 'series', 'open_price', 'high_price', 'lo
                      'total_traded_qty', 'total_traded_value', 'high_52week', 'low_52week', 'total_trades', 'date', 'created_at', 'updated_at']
 
 class StockService:
+
+    @classmethod
+    def get_equity_history(cls, symbol,series,start_date,end_date):
+        """
+        This method fetches the historical data for the given stock symbol.
+        :param symbol: ticker symbol
+        :param series: 'EQ' for equity, 'BE' for bond
+        :param start_date: start date for the historical data
+        :param end_date: end date for the historical data
+        :return: historical data for the given stock symbol
+        """
+        try:
+            df = equity_history(symbol,series,start_date,end_date)
+            df = cls.map_stock_df_columns(df)
+            df['date'] = pd.to_datetime(df['date']).dt.date
+            df['date'] = pd.to_datetime(df['date']).dt.strftime(striped_date_format)
+            df = cls.check_duplidate_sotck_record(df)
+            
+            # Fill the missing data with previous day's closing price
+            # reference: https://stackoverflow.com/questions/38361526/fill-the-missing-date-values-in-a-pandas-dataframe-column
+            df['date'] =  pd.to_datetime(df['date'], format=striped_date_format)
+            df = df.sort_values(by=['date'], ascending=[True])
+            df.set_index('date', inplace=True)
+            df = df.resample('D').ffill().reset_index()
+            df['date'] = df['date'].dt.strftime(striped_date_format)
+            return df
+        except Exception as e:
+            print(e)
+            return None
+
+
     @classmethod
     def check_duplidate_sotck_record(cls,stock_df):
         stock_df = stock_df.drop_duplicates(subset=['symbol','date'])
@@ -27,22 +58,14 @@ class StockService:
             stock_df['date'] = pd.to_datetime(stock_df['date']).dt.strftime(striped_date_format)
         except Exception as e:
             print(e)
-            df = equity_history(symbol,series,start_date,end_date)
-            df = cls.map_stock_df_columns(df)
-            df['date'] = pd.to_datetime(df['date'], format = '%Y-%m-%d %H:%M:%S').dt.date
-            df['date'] = pd.to_datetime(df['date']).dt.strftime(striped_date_format)
-            df = cls.check_duplidate_sotck_record(df)
+            df = cls.get_equity_history(symbol,series,start_date,end_date)
             return df
         
         # if misisng min or max date, then fetch the data from nse
         if stock_df.empty:
-            df = equity_history(symbol,series,start_date,end_date)
-            df = cls.map_stock_df_columns(df)
-            df['date'] = pd.to_datetime(df['date'], format = '%Y-%m-%d %H:%M:%S').dt.date
-            df['date'] = pd.to_datetime(df['date']).dt.strftime(striped_date_format)
-            df = cls.check_duplidate_sotck_record(df)
+            df = cls.get_equity_history(symbol,series,start_date,end_date)
             return df
-        
+
         max_date_dt = pd.to_datetime(stock_df['date'], format = "%d-%m-%Y").max()
         min_date_dt = pd.to_datetime(stock_df['date'], format = "%d-%m-%Y").min()
         start_date_dt = datetime.strptime(start_date, "%d-%m-%Y")
@@ -56,11 +79,7 @@ class StockService:
         elif max_date_dt < end_date_dt:
             start_date = max_date_dt.strftime(striped_date_format)
         # todo: fetch only the required data from nse and append to the existing data
-        df = equity_history(symbol,series,start_date,end_date)
-        df = cls.map_stock_df_columns(df)
-        df['date'] = pd.to_datetime(df['date'], format = '%Y-%m-%d %H:%M:%S').dt.date
-        df['date'] = pd.to_datetime(df['date']).dt.strftime(striped_date_format)
-        df = cls.check_duplidate_sotck_record(df)
+        df = cls.get_equity_history(symbol,series,start_date,end_date)
         return df
 
     @classmethod
@@ -100,7 +119,7 @@ class StockService:
                                   'CH_LAST_TRADED_PRICE':'last_price', 'CH_PREVIOUS_CLS_PRICE':'prev_close_price',
                                   'CH_TOT_TRADED_QTY':'total_traded_qty', 'CH_TOT_TRADED_VAL':'total_traded_value',
                                   'CH_52WEEK_HIGH_PRICE' : 'high_52week', 'CH_52WEEK_LOW_PRICE':'low_52week',
-                                 'CH_TOTAL_TRADES' : 'total_trades','TIMESTAMP': 'date',
+                                 'CH_TOTAL_TRADES' : 'total_trades','CH_TIMESTAMP': 'date',
                                   'createdAt': 'created_at', 'updatedAt' : 'updated_at'
                                   }, inplace=True)
 
