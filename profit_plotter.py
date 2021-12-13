b999d78fa6d6ce1472f1b11d0d73ffcdc3f34a0c
rsohlot <5600987+rsohlot@users.noreply.github.com>
2021-12-14 01:20:23 +0530

    feature:add checklist with callback fun

--- profit_plotter.py
+++ profit_plotter.py
@@ -38,6 +38,20 @@ fig = px.line(data, x="date", y=data.columns[1:])
 fig.update_traces(mode="markers+lines", hovertemplate=None)
 fig.update_layout(hovermode="x")
 
+# create data for app
+# todo: store stoke in a seperate place
+stock_checklist_dict = []
+stock_checklist_values = []
+stock_cols = profit_col
+for element in ['daily_profit_and_loss', 'day_profit_status']:
+    if element in stock_cols:
+        stock_cols.remove(element)
+
+for each_stock in stock_cols:
+    each_stock_name = each_stock.split('_')[0]
+    stock_checklist_dict.append({'label': each_stock_name, 'value': each_stock_name})
+    stock_checklist_values.append(each_stock_name)
+
 # App for plotting
 app = dash.Dash(
     __name__,
@@ -46,17 +60,29 @@ app = dash.Dash(
 server = app.server
 
 app.layout = html.Div([
+    #stock check list
+    dcc.Checklist(
+        options=stock_checklist_dict,
+                value=stock_checklist_values,
+                labelStyle={'display': 'inline-block'}
+            , id='stock_checklist'),
     # graph
-     dcc.Graph(figure=fig),
+     dcc.Graph(figure=fig, id='portfolio-profit-graph'),
 ], id='main-div', style = {'display': 'inline-block','height' : '95%', 'width': '95%'})
 
 
-# @app.callback(
-#     Output("line-chart", "figure"))
-# def update_line_chart():
-#     fig = px.line(data, 
-#         x="date", y="day_profit_status")
-#     return fig
+@app.callback(Output("portfolio-profit-graph", "figure"), Input("stock_checklist", "value"))
+def update_line_chart(stocks_selected):
+    #filter the data
+    req_col = ['date', 'daily_profit_and_loss', 'day_profit_status']
+    for each_stock in stocks_selected:
+        each_stock = each_stock + '_profit'
+        req_col.append(each_stock)
+    updated_data  = data[req_col]
+    fig = fig = px.line(updated_data, x="date", y=updated_data.columns[1:])
+    fig.update_traces(mode="markers+lines", hovertemplate=None)
+    fig.update_layout(hovermode="x")
+    return fig
 
 app.run_server(debug=True)
 
